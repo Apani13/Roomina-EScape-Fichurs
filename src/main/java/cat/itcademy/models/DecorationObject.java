09a66e6e956cba15c42b2a16bb31769bd600d5a9
mxg952 <mguillempa@proton.me>
2025-09-22 18:21:34 +0200

    feat: Create Test to thrown duplicate exception

--- src/main/java/cat/itcademy/models/DecorationObject.java
+++ src/main/java/cat/itcademy/models/DecorationObject.java
@@ -26,5 +26,19 @@ public class DecorationObject {
     public long getQuantity() {
         return quantity;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DecorationObject that = (DecorationObject) o;
+        return name != null && name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 }
+
 
