3b52358b704dd45222ec04701de2a0f70d7de18e
mxg952 <mguillempa@proton.me>
2025-09-22 17:58:49 +0200

    feature: Decoration Objects creation Test and implementation

---
+++ src/main/java/cat/itcademy/utils/ObjectErrorMessages.java
@@ -0,0 +1,11 @@
+package cat.itcademy.utils;
+
+public class ObjectErrorMessages {
+
+    private ObjectErrorMessages() {}
+
+    public static final String OBJECT_DUPLICATED = "El objeto ya existe.";
+    public static final String OBJECT_NAME_NULL_EMPTY = "El nombre del objeto está vacío o es nulo.";
+    public static final String OBJECT_MATERIAL_NULL_EMPTY = "El  está vacío o es nulo.";
+    public static final String OBJECT_QUANTITY_INVALID = "El nombre de la sala está vacío o es nulo.";
+}
