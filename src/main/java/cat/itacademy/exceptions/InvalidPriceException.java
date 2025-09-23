b260acfa8fb963b2736c55ae336d2563fab90988
Andrea <andreapan98@gmail.com>
2025-09-23 10:52:58 +0200

    feat(clue): refactor

--- src/main/java/cat/itacademy/exceptions/InvalidPriceException.java
+++
@@ -1,7 +0,0 @@
-package cat.itacademy.exceptions;
-
-public class InvalidPriceException extends RuntimeException {
-    public InvalidPriceException(String message) {
-        super(message);
-    }
-}
