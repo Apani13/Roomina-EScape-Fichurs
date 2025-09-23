31201af9454175aa0e2c63edc667340d949267d1
luriguso <luriguso@gmail.com>
2025-09-23 13:23:10 +0200

    refactor: apply code normalization

--- src/main/java/cat/itacademy/exceptions/InvalidNameException.java
+++
@@ -1,7 +0,0 @@
-package cat.itacademy.exceptions;
-
-public class InvalidNameException extends RuntimeException {
-    public InvalidNameException(String message) {
-        super(message);
-    }
-}
