181bda89f393ab01169d2829853d41d8815996c9
mxg952 <mguillempa@proton.me>
2025-09-23 10:59:34 +0200

    Create SuccessMessages class

--- src/main/java/cat/itcademy/exceptions/InvalidAtributeException.java
+++
@@ -1,7 +0,0 @@
-package cat.itcademy.exceptions;
-
-public class InvalidAtributeException extends RuntimeException {
-    public InvalidAtributeException(String message) {
-        super(message);
-    }
-}
