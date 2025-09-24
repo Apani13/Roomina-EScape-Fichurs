a642a1bad2707f2aa5051620c8d8afcaf830c356
Andrea <andreapan98@gmail.com>
2025-09-24 22:51:12 +0200

    Revert "Merge pull request #7 from develop"

--- src/main/java/cat/itacademy/models/Room.java
+++ src/main/java/cat/itacademy/models/Room.java
@@ -1,37 +1,40 @@
 package cat.itacademy.models;
 
-public class DecorationObject {
+public class Room {
 
     private String name;
-    private String material;
+    private String theme;
+    private int level;
     private double price;
-    private long quantity;
 
-    public DecorationObject(String name, String material, long quantity) {
+    public Room(String name, String theme, int level) {
         this.name = name;
-        this.material = material;
-        this.quantity = quantity;
-        this.price = 5;
-
+        this.theme = theme;
+        this.level = level;
+        this.price = 25.0;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getMaterial() {
-        return material;
+    public String getTheme() {
+        return theme;
     }
 
-    public long getQuantity() {
-        return quantity;
+    public int getLevel() {
+        return level;
+    }
+
+    public double getPrice() {
+        return price;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        DecorationObject that = (DecorationObject) o;
+        Room that = (Room) o;
         return name != null && name.equals(that.name);
     }
 
@@ -40,5 +43,3 @@ public class DecorationObject {
         return name != null ? name.hashCode() : 0;
     }
 }
-
-
