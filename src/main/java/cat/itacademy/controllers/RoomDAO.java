6cd913cfd8a37e6c5caa6b3d4cfb8b383d5e9402
mxg952 <mguillempa@proton.me>
2025-09-26 13:10:54 +0200

    feat: Implement RommDAO

--- src/main/java/cat/itacademy/controllers/RoomDAO.java
+++ src/main/java/cat/itacademy/controllers/RoomDAO.java
@@ -1,23 +1,41 @@
 package cat.itacademy.controllers;
 
 import cat.itacademy.models.Room;
+import cat.itacademy.repositories.DatabaseConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class RoomDAO {
 
-
-    public void save(Room room) throws SQLException {
-        String sql = "INSERT INTO escaperoom (name, theme, level) VALUES(?,?,?)";
+    public void insert(Room room) throws SQLException {
+        String sql = "INSERT INTO room (name, theme, level, price) VALUES(?,?,?,?)";
 
         try (Connection conn = DatabaseConnection.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setString(1,room.getName());
             stmt.setString(2, room.getTheme());
             stmt.setInt(3,room.getLevel());
+            stmt.setDouble(4,room.getPrice());
             stmt.executeUpdate();
         }
     }
+
+    public boolean existsByName(String name) throws SQLException {
+        String sql = "SELECT COUNT(*) FROM room WHERE name = ?";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, name);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt(1) > 0;
+                }
+            }
+        }
+        return false;
+
+    }
+
 }
